fcd32d2c12f819e15eb6bd803c3a8bcacde2baea
Jorge Gómez <gomezjorge.exequiel@gmail.com>
2015-05-03 12:49:26 -0300

    Modificaciones en la funcion de testing de valores de eta,etainc y etadec. Subidos datos, en data con una corrida, en dataavg2 corrido con 4 corridas, y en dataavg tambien, pero no llego a terminar.

--- graphEtaTest.m
+++ graphEtaTest.m
@@ -15,6 +15,7 @@ function graphEtaTest(incstart,incend,incstep,decstart,decend,decstep,filename)
 				w2 = mperceptron(1,1,neurons,100,eta,@activation,@activationD,0,0,0,0,0.9,1,i,j);
 				w3 = mperceptron(1,1,neurons,100,eta,@activation,@activationD,0,0,0,0,0.9,1,i,j);
 				w4 = mperceptron(1,1,neurons,100,eta,@activation,@activationD,0,0,0,0,0.9,1,i,j);
+				printf("Errores %f %f %f %f\n",w1{3},w2{3},w3{3},w4{3});
 				sumerror = [w1{3},w2{3},w3{3},w4{3}];
 				avgerror = mean(sumerror);
 				fprintf(file_id,"Para eta=%f etainc=%f etadec=%f iteraciones=%d el error es=%f.\n",eta,i,j,iterations,avgerror);
